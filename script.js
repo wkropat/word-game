50a8555af27f5221f1a8d6d12d3326cc7c521c43
wkropat <wkropat@gmail.com>
2021-09-24 14:15:54 -0700

    make divs autopopulate inside the gameBoard

--- script.js
+++ script.js
@@ -24,8 +24,10 @@ function startGames() {
 
     // Make div for each letter in word, put in the gameBoard div
     for (let i=1; i < word.length(); i++) {
-
-       // display:none initially
+        var guessBox = document.createElement(div);
+        guessBox.textContent(word[i])
+        guessBox.setAttribute("style", "display:none")
+        document.getElementById("#gameBoard").appendChild(div);
     }
 
 
@@ -67,31 +69,3 @@ function guessLetters() {
 
 }
 
-// Stores user response in variable
-var tagName = prompt("Please enter an HTML Tag (ex. h1, h2, p, div):", "enter tag");
-
-if (tagName !== "h1" && tagName !== "h2" && tagName !== "p" && tagName !== "div") {
-  alert("please enter a valid tag");
-} else {
-  // Creates element based on tag entered by user
-  var tag = document.createElement(tagName);
-
-  // Adds text content to created tag
-  tag.textContent = "This was made via prompts. It's a " + tagName + ".";
-  
-  // Appends tag as child of document body
-  document.body.appendChild(tag);
-}
-
-var nextTag = confirm("Would you like to add another tag?");
-
-if (nextTag === true) {
-  var secondTagName = prompt("Please enter another  HTML Tag (ex. h1, h2, p, div):", "enter tag here");
-  if(secondTagName !== "h1" && secondTagName !== "h2" && secondTagName !== "p" && secondTagName !== "div") {
-    alert("please enter a valid tag");
-  } else {
-    var secondTag = document.createElement(secondTagName);
-    secondTag.textContent = "This is our second tag via prompts, it's a " + secondTagName + ".";
-    document.body.appendChild(secondTag);
-  }
-}
